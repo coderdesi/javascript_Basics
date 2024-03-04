8fb4f455ce1355bec0c0d882e048b3b49d17571b
sauravkumarrajak <sauravrajak@outlook.com>
2024-03-04 10:30:27 +0530

    added some notes in '01_basics/01_variable.js'

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,5 +1,14 @@
-"use strict";  //treat all JS code as newer version 
+"use strict"; //treat all JS code as newer version
 
 // alert(3+3) // we are using node js , not broser
 
-console.log(2+3)
+console.log(2 + 3);
+
+// Data Types in JavaScript
+
+// 1. Primitive Data Types
+// string
+// number
+// boolean
+// null
+// undefined
